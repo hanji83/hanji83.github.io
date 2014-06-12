0c94c765302cc93f03f394db906a917ea989b177
hanji83 <andrew.ac.chen@gmail.com>
2014-06-11 22:13:07 -0700

    Initial commit

---
+++ contactthanks.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<meta charset="utf-8">
+	<title>Devito Resume</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Le styles -->
+	<link href="assets/css/bootstrap.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Leckerli+One' rel='stylesheet' type='text/css'>
+
+	<link href='http://fonts.googleapis.com/css?family=Lato:400,900,700italic' rel='stylesheet' type='text/css'> 
+	<link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700' rel='stylesheet' type='text/css'>
+
+	<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+	<link href="assets/css/style.css" rel="stylesheet">
+	<link rel="stylesheet" href="assets/css/jquery.shadow.css" />
+	<link href="assets/css/jquery.tweet.css" rel="stylesheet"/>
+	<link rel="stylesheet" type="text/css" href="assets/css/jquery.lightbox-0.5.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="assets/css/jquery.fancybox.css" media="screen" />
+  
+
+  
+  
+	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+	  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Le fav and touch icons -->
+	<link rel="shortcut icon" href="assets/ico/favicon.ico">
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+  </head>
+
+  <body>
+
+<div class="container-fluid wrapper">
+	<div class="row-fluid">
+		<div class="span12">
+
+				<div id="header"><!-- Header -->
+	  <div class="logo"><p>JD</p></div>
+		  <h1>John Eriksson</h1>
+		  <h2>Art Director And Designer</h2>
+		  		
+	 </div>
+
+    <div class="social_panel hidden-phone">
+	<img src="assets/img/print.png" alt=""><a href="javascript:window.print()">PRINT RESUME</a>	  
+	</div>
+	
+	<div class="contact-panel hidden-phone">
+	<h5>Call Me :</h5>+62 8175 278 69  
+	</div> 
+	
+    <div class="ribbon hidden-phone"><!-- Ribbon Menu -->
+    <div class="ribbon-left"></div>
+        <ul class="menu-ribbon">
+				<li class="current"><a href="index.html"><i class="icon-search icon-white"></i> Back to Home</a></li>
+        </ul>
+      <div class="ribbon-right"></div>
+    </div>
+		
+		</div>
+	</div>
+
+</div><!-- end container -->
+			<div class="container-fluid wrapper2">
+					<div class="row-fluid">
+					<div class="span12">
+
+
+				
+				<div class="hero-unit">
+              <h1>Hiya!</h1>
+              <p>YOUR MESSAGE HAS BEEN SENT!</p>
+              <p><a href="index.html" class="btn btn-inverse btn-large">Back to Home</a></p>
+            </div>
+				
+				
+				
+				
+					
+					</div>
+				</div>
+			</div>
+	<div class="container-fluid wrapper-footer">
+	<div class="row-fluid">
+
+				<div class="span9">Copyright &copy; 2014 <a href="#">matirasa</a> - Just another Resume sitePowered by Boostrap </div>
+					<div class="span3">
+						<ul class="social-media">
+							<li><a class="blue" href="#"><img width="28" height="28" alt="Facebook Profile" src="assets/ico/facebook.png"></a></li>
+							<li><a class="blue" href="#"><img width="28" height="28" alt="Twitter Profile" src="assets/ico/twitter.png"></a></li>
+							<li><a class="blue" href="#"><img width="28" height="28" alt="Linked In Profile" src="assets/ico/rss.png"></a></li>
+						</ul>
+					</div>
+			
+	</div>
+	</div>
+<div class="wrapper-footer-shadow"></div>
+	<!-- Le javascript
+	================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="assets/js/jquery.js"></script>	
+
+  </body>
+</html>
